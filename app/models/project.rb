4984941e14529e308e97aff5ed1fa96a8faa8488
Sandy Rodger <abmrodger@gmail.com>
2025-12-10 14:57:36 +0000

    fix enum

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  enum status: { planned: 0, in_progress: 1, done: 2 }, _default: :planned
+  enum status: { planned: 0, in_progress: 1, done: 2 }
 
   scope :highlighted, -> { where(highlight: true).order(:position) }
   scope :ordered,     -> { order(:position) }
