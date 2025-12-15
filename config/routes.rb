f20365e7533c178360ff00362e672da34f176ac0
Sandy Rodger <abmrodger@gmail.com>
2025-12-15 14:07:19 +0000

    add CV page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "/cv", to: "pages#cv"
+  
 end
