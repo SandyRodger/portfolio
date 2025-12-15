7ce161cd0f28f6a9ea1f6ede5da53b57b407a1f8
Sandy Rodger <abmrodger@gmail.com>
2025-12-15 16:30:36 +0000

    new simplified landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resource :home, only: :show
   resource :projects, only: [ :show ]
   resources :contributions, only: :index
-
+  
+  # get "/music-code/spiegel-im-spiegel", to: "pages#spiegel", as: :spiegel
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
   get "up" => "rails/health#show", as: :rails_health_check
